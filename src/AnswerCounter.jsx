5c73ed31e235fe5154501cd9a98878b9e2440801
peynadol <peynadol@gmail.com>
2025-03-27 13:27:07 +0000

    add correct counter

---
+++ src/AnswerCounter.jsx
@@ -0,0 +1,7 @@
+export default function AnswerCounter(props) {
+  return (
+    <>
+      <p>{props.correctCount}</p>
+    </>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import FlagCard from "./FlagCard";
 import GuessInput from "./GuessInput";
+import AnswerCounter from "./AnswerCounter";
 
 const BASE_URL = "https://restcountries.com/v3.1/";
 
@@ -9,11 +10,13 @@ function App() {
   const [countryInfo, setCountryInfo] = useState(null);
   const [inputValue, setInputValue] = useState("");
   const [userGuess, setUserGuess] = useState("");
+  const [totalQuestionCount, setTotalQuestionCount] = useState("");
+  const [correctCount, setCorrectCount] = useState(0);
 
   useEffect(() => {
     const getCountry = async () => {
       try {
-        const response = await fetch(`${BASE_URL}name/finland`);
+        const response = await fetch(`${BASE_URL}name/spain`);
         if (!response.ok) throw new Error("Failed to fetch");
         const [data] = await response.json();
         setCountryInfo(data);
@@ -31,18 +34,21 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const isCorrect = checkCorrect(inputValue);
+    if (isCorrect) {
+      setCorrectCount((prev) => prev + 1);
+    }
     setUserGuess(inputValue);
-    checkCorrect();
 
     setInputValue("");
   };
 
-  const checkCorrect = () => {
-    if (!userGuess || !countryInfo) return false;
+  const checkCorrect = (guess) => {
+    if (!guess || !countryInfo) return false;
     const commonName = countryInfo.name.common.toLowerCase();
     const officialName = countryInfo.name.official.toLowerCase();
     const allNames = [commonName, officialName];
-    return allNames.includes(userGuess.toLowerCase().trim());
+    return allNames.includes(guess.toLowerCase().trim());
   };
 
   return (
@@ -60,6 +66,7 @@ function App() {
       ) : (
         <p>{`Incorrect`}</p>
       )}
+      <AnswerCounter correctCount={correctCount} />
     </>
   );
 }
