102c6276418bcb48419ec17f91b28b2cf3e86e77
peynadol <peynadol@gmail.com>
2025-03-27 12:50:08 +0000

    add check user correct

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,22 @@ const BASE_URL = "https://restcountries.com/v3.1/";
 
 function App() {
   const [countryInfo, setCountryInfo] = useState(null);
-  const [flag, setFLag] = useState(null);
   const [inputValue, setInputValue] = useState("");
-  const [userGuess, setUserGuess] = useState(null);
+  const [userGuess, setUserGuess] = useState("");
+
   useEffect(() => {
-    const getFlag = async () => {
+    const getCountry = async () => {
       try {
-        const response = await fetch(`${BASE_URL}name/sweden`);
+        const response = await fetch(`${BASE_URL}name/finland`);
         if (!response.ok) throw new Error("Failed to fetch");
         const [data] = await response.json();
-        setFLag({
-          png: data.flags.png,
-          alt: data.flags.alt,
-        });
+        setCountryInfo(data);
       } catch (err) {
         console.error("Fetch error:", err);
-        setFLag(null);
+        setCountryInfo(null);
       }
     };
-    getFlag();
+    getCountry();
   }, []);
 
   const handleInputChange = (event) => {
@@ -35,20 +32,34 @@ function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
     setUserGuess(inputValue);
+    checkCorrect();
+
     setInputValue("");
+  };
+
+  const checkCorrect = () => {
+    if (!userGuess || !countryInfo) return false;
+    const commonName = countryInfo.name.common.toLowerCase();
+    const officialName = countryInfo.name.official.toLowerCase();
+    const allNames = [commonName, officialName];
+    return allNames.includes(userGuess.toLowerCase().trim());
   };
 
   return (
     <>
-      {flag ? <FlagCard flag={flag} /> : undefined}
-      {flag ? (
+      {countryInfo ? <FlagCard flag={countryInfo.flags} /> : undefined}
+      {countryInfo ? (
         <GuessInput
           value={inputValue}
           onChange={handleInputChange}
           onSubmit={handleSubmit}
         />
       ) : undefined}
-      {userGuess ? <p>{`Your guess was ${userGuess}`}</p> : undefined}
+      {!userGuess ? undefined : checkCorrect() ? (
+        <p>{`Correct`}</p>
+      ) : (
+        <p>{`Incorrect`}</p>
+      )}
     </>
   );
 }
