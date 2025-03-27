ff8ca3add859b16770f44bbad3bae9530814bd1c
peynadol <peynadol@gmail.com>
2025-03-27 14:06:44 +0000

    add functionality to grab 5 random countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import arrayShuffle from "array-shuffle";
 
 import FlagCard from "./FlagCard";
 import GuessInput from "./GuessInput";
@@ -7,26 +8,37 @@ import AnswerCounter from "./AnswerCounter";
 const BASE_URL = "https://restcountries.com/v3.1/";
 
 function App() {
-  const [countryInfo, setCountryInfo] = useState(null);
+  const [allCountries, setAllCountries] = useState([]);
+  const [gameRoundCountries, setGameRoundCountries] = useState([]);
+  const [currentCountryIndex, setCurrentCountryIndex] = useState(0);
   const [inputValue, setInputValue] = useState("");
   const [userGuess, setUserGuess] = useState("");
   const [totalQuestionCount, setTotalQuestionCount] = useState("");
   const [correctCount, setCorrectCount] = useState(0);
 
   useEffect(() => {
-    const getCountry = async () => {
+    const getAllCountries = async () => {
       try {
-        const response = await fetch(`${BASE_URL}name/spain`);
+        const response = await fetch(`${BASE_URL}all`);
         if (!response.ok) throw new Error("Failed to fetch");
-        const [data] = await response.json();
-        setCountryInfo(data);
+        const data = await response.json();
+        setAllCountries(data);
       } catch (err) {
         console.error("Fetch error:", err);
-        setCountryInfo(null);
+        setAllCountries(null);
       }
     };
-    getCountry();
+    getAllCountries();
   }, []);
+
+  useEffect(() => {
+    if (allCountries.length > 0) {
+      const shuffled = arrayShuffle(allCountries);
+      const firstFive = shuffled.slice(0, 5);
+      setGameRoundCountries(firstFive);
+      setCurrentCountryIndex(0);
+    }
+  }, [allCountries]);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -43,18 +55,24 @@ function App() {
     setInputValue("");
   };
 
+  console.log(gameRoundCountries);
+
   const checkCorrect = (guess) => {
-    if (!guess || !countryInfo) return false;
-    const commonName = countryInfo.name.common.toLowerCase();
-    const officialName = countryInfo.name.official.toLowerCase();
+    if (!guess || !gameRoundCountries[currentCountryIndex]) return false;
+    const commonName =
+      gameRoundCountries[currentCountryIndex].name.common.toLowerCase();
+    const officialName =
+      gameRoundCountries[currentCountryIndex].name.official.toLowerCase();
     const allNames = [commonName, officialName];
     return allNames.includes(guess.toLowerCase().trim());
   };
 
   return (
     <>
-      {countryInfo ? <FlagCard flag={countryInfo.flags} /> : undefined}
-      {countryInfo ? (
+      {gameRoundCountries[currentCountryIndex] ? (
+        <FlagCard flag={gameRoundCountries[currentCountryIndex].flags} />
+      ) : undefined}
+      {gameRoundCountries[currentCountryIndex] ? (
         <GuessInput
           value={inputValue}
           onChange={handleInputChange}
